39c8831aa284ca379ecf99d2ece804f10ecc9b7d
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-03-19 13:47:01 +0700

    Add files via upload

---
+++ khai bao su dung bien/src/number.java
@@ -0,0 +1,17 @@
+public class number {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = false;
+        char c = 'b';
+        String s = "Ho Chi Minh";
+        System.out.println("i= "+i);
+        System.out.println("f= "+f);
+        System.out.println("d= "+d);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+        System.out.println("s= "+s);
+
+    }
+}
